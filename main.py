65fd43b38af060f8ce9e8a5d022f9bbd7c0b27b7
ilyakolbafk <ivkolb@edu.hse.ru>
2022-01-05 22:21:27 +0300

    Add example

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from field import *
 
+# Random play
 game = Field()
-game.generate(5)
+game.generate(6)
 game.play()
+
+# Beautiful example.
+# game = Field()
+# game.append(20, 20)
+# game.append(21, 20)
+# game.append(20, 21)
+# game.append(20, 22)
+# game.append(20, 23)
+# game.play()
